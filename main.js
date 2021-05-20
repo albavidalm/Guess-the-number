9002c7ecf7c56b69789fdbb524d90182543bfba9
Alba V <albavidalm@gmail.com>
2021-05-20 19:59:00 +0200

    Add final code

--- main.js
+++ main.js
@@ -1,32 +1,42 @@
 "use strict";
-
-console.log("Working");
-
-//Genera Random Number e imprime en la consola
-const randomNumber = getRandomNumber(100);
-
+// Función para generar el número Random:
 function getRandomNumber(max) {
   return Math.ceil(Math.random() * max);
 }
-console.log(randomNumber);
+const randomNumber = getRandomNumber(100);
+console.log("Número random:", randomNumber);
+//___________________________________________________________
 
-//_________________________
+// Comparaciones:
+const button = document.querySelector(".js-button");
+const clue = document.querySelector(".js-clue");
+const userNumber = document.querySelector(".js-input");
 
-// Recogemos el input de la user y lo transformamos a número
-const inputNumber = document.querySelector(".js-input");
-const inputValue = parseInt(inputNumber.value);
+const compare = () => {
+  const userNumber2 = userNumber.value;
+  const userNumberValue = parseInt(userNumber2);
+  console.log(`Número de la usuaria: ${userNumberValue}`);
+  if (userNumberValue === randomNumber) {
+    clue.innerHTML = "Has ganado campeona!!!";
+  } else if (userNumber2 < 1 || userNumber2 > 100) {
+    clue.innerHTML = `El número debe estar entre 1 y 100.`;
+  } else if (userNumber2 < randomNumber) {
+    clue.innerHTML = "Demasiado bajo";
+  } else if (userNumber2 > randomNumber) {
+    clue.innerHTML = "Demasiado alto";
+  }
+};
+button.addEventListener("click", compare);
+//___________________________________________________________
 
-//
-
-//Contador----
-
-const buttonSender = document.querySelector(".js-button");
+// Contador de intentos:
 const userCounter = document.querySelector(".js-counter");
 let counter = 0;
 function countTries() {
   counter += 1;
   //console.log(`${counter}`);
-  userCounter.value = userCounter.innerHTML + counter;
+  userCounter.innerHTML = `Número de intentos: ${counter}`;
 }
 
-buttonSender.addEventListener("click", countTries);
+button.addEventListener("click", countTries);
+//___________________________________________________________
